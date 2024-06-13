3a0046fe133dae6da4a0eb5271aa4f9e13e39f60
AjaySRawat07 <ajju246455@gmail.com>
2024-06-13 23:30:47 +0530

    first

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import logo from "./logo.png";
+import darkLogo from "./darkLogo.png";
+import banner_1 from './banner/banner_1.jpg'
+import banner_2 from './banner/banner_2.jpg'
+import banner_3 from './banner/banner_3.jpg'
+import banner_4 from './banner/banner_4.jpg'
+import banner_5 from './banner/banner_5.jpg'
+import emptyCart from "./emptyCart.png";
+import indFlag from "./indFlag.jpeg"
+
+export {
+  logo,
+  darkLogo,
+  banner_1,
+  banner_2,
+  banner_3,
+  banner_4,
+  banner_5,
+  emptyCart,
+  indFlag,
+};
---
+++ src/firebase.confige.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoP_77CekXA5bkQuHEwuvQ7VL9aCQ56ew",
+  authDomain: "data-5d5fa.firebaseapp.com",
+  projectId: "data-5d5fa",
+  storageBucket: "data-5d5fa.appspot.com",
+  messagingSenderId: "158368408111",
+  appId: "1:158368408111:web:4b086e914165f474219cda",
+  measurementId: "G-68K4XZX1QJ"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
